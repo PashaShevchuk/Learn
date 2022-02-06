857e7dea2e5a7469f87a401b2e8ece757c0d1be5
pavlo-shevchuk <pavlo.shevchuk@sombrainc.com>
2022-02-06 22:45:08 +0200

    -

--- event_loop/eventloop.js
+++ event_loop/eventloop.js
@@ -1,24 +1,24 @@
-// const fs = require('fs');
-//
-// fs.readFile(__filename, () => {
-//   console.log('readFile');
-//
-//   setTimeout(() => console.log('timeout1'));
-//   setImmediate(() => console.log('immediate1'));
-//   Promise.resolve()
-//     .then(() => console.log('Promise.resolve1'));
-//   process.nextTick(() => console.log('process.nextTick1'));
-// });
-//
-// setTimeout(() => console.log('timeout2'));
-// setImmediate(() => console.log('immediate2'));
-//
-// Promise.resolve()
-//   .then(() => console.log('Promise.resolve2'));
-//
-// process.nextTick(() => console.log('process.nextTick2'));
-//
-// console.log('sync code');
+const fs = require('fs');
+
+fs.readFile(__filename, () => {
+  console.log('readFile');
+
+  setTimeout(() => console.log('timeout1'));
+  setImmediate(() => console.log('immediate1'));
+  Promise.resolve()
+    .then(() => console.log('Promise.resolve1'));
+  process.nextTick(() => console.log('process.nextTick1'));
+});
+
+setTimeout(() => console.log('timeout2'));
+setImmediate(() => console.log('immediate2'));
+
+Promise.resolve()
+  .then(() => console.log('Promise.resolve2'));
+
+process.nextTick(() => console.log('process.nextTick2'));
+
+console.log('sync code');
 
 // sync code
 // process.nextTick2
@@ -31,34 +31,34 @@
 // immediate1
 // timeout1
 
-const fs = require('fs');
-
-function main() {
-  setTimeout(() => console.log('1'), 0);
-  setImmediate(() => console.log('2'));
-
-  fs.readFile('./xyz.txt', (err, buff) => {
-    setTimeout(() => {
-      console.log('3');
-    }, 1000);
-
-    process.nextTick(() => {
-      console.log('process.nextTick');
-    });
-
-    setImmediate(() => console.log('4'));
-  });
-
-  setImmediate(() => console.log('5'));
-
-  setTimeout(() => {
-    process.on('exit', (code) => {
-      console.log(`close callback`);
-    });
-  }, 1100);
-}
-
-main();
+// const fs = require('fs');
+//
+// function main() {
+//   setTimeout(() => console.log('1'), 0);
+//   setImmediate(() => console.log('2'));
+//
+//   fs.readFile('./xyz.txt', (err, buff) => {
+//     setTimeout(() => {
+//       console.log('3');
+//     }, 1000);
+//
+//     process.nextTick(() => {
+//       console.log('process.nextTick');
+//     });
+//
+//     setImmediate(() => console.log('4'));
+//   });
+//
+//   setImmediate(() => console.log('5'));
+//
+//   setTimeout(() => {
+//     process.on('exit', (code) => {
+//       console.log(`close callback`);
+//     });
+//   }, 1100);
+// }
+//
+// main();
 
 // 1
 // process.nextTick
--- event_loop/index.js
+++ event_loop/index.js
@@ -1,28 +1,28 @@
-// const fs = require('fs');
-//
-// console.log('START');
-//
-// setTimeout(() => console.log('setTimeout 1'), 0);
-//
-// setImmediate(() => console.log('setImmediate'));
-//
-// fs.readFile('text_file.txt', () => {
-//   setTimeout(() => console.log('readFile setTimeout'), 0);
-//   setImmediate(() => console.log('readFile setImmediate'));
-//   process.nextTick(() => console.log('readFile nextTick'));
-// });
-//
-// Promise.resolve()
-//   .then(() => {
-//     console.log('Promise');
-//     process.nextTick(() => console.log('Promise nextTick'));
-//   });
-//
-// process.nextTick(() => console.log('nextTick'));
-//
-// setTimeout(() => console.log('setTimeout 2'), 0);
-//
-// console.log('END');
+const fs = require('fs');
+
+console.log('START');
+
+setTimeout(() => console.log('setTimeout 1'), 0);
+
+setImmediate(() => console.log('setImmediate'));
+
+fs.readFile('text_file.txt', () => {
+  setTimeout(() => console.log('readFile setTimeout'), 0);
+  setImmediate(() => console.log('readFile setImmediate'));
+  process.nextTick(() => console.log('readFile nextTick'));
+});
+
+Promise.resolve()
+  .then(() => {
+    console.log('Promise');
+    process.nextTick(() => console.log('Promise nextTick'));
+  });
+
+process.nextTick(() => console.log('nextTick'));
+
+setTimeout(() => console.log('setTimeout 2'), 0);
+
+console.log('END');
 
 // 1 цикл:
 // START
